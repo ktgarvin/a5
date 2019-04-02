e3a1509d32cad3d95c6185d1878e545a5fa9b29b
ktshep <ktshep@192.168.100.235>
2019-04-02 17:00:42 -0400

    s

--- src/a5/GoldPlate.java
+++ src/a5/GoldPlate.java
@@ -1,5 +1,7 @@
 package a5;
 
+//GoldPlate implements the Plate Interface
+//GoldPlate is a plate of Sushi with potentially 1 Sushi object
 public class GoldPlate implements Plate {
 	
 	private double price;
@@ -14,6 +16,9 @@ public class GoldPlate implements Plate {
 	}
 
 	@Override
+//	gets the content of the Sushi plate
+//	input: no parameter
+//	output: null or 1 Sushi object
 	public Sushi getContents() {
 		if (contents == null) {
 			return null;
@@ -23,6 +28,9 @@ public class GoldPlate implements Plate {
 	}
 
 	@Override
+//	removes the contents of the Sushi plate 
+//	output: null or 1 Sushi object
+//	returns the original contents of the plate and then empties the plate
 	public Sushi removeContents() {
 		Sushi oldContents = contents;
 		contents = null;
@@ -30,6 +38,9 @@ public class GoldPlate implements Plate {
 	}
 
 	@Override
+//	sets the contents of the Sushi plate to a new Sushi object
+//	input: a new Sushi object
+//	output: void but replaces the contents of the original Sushi plate
 	public void setContents(Sushi s) throws PlatePriceException {
 		if (s == null) {
 			throw new IllegalArgumentException("s is null!");
@@ -41,6 +52,7 @@ public class GoldPlate implements Plate {
 	}
 
 	@Override
+//	returns true if the Sushi plate has a Sushi object on it
 	public boolean hasContents() {
 		if (contents != null) {
 			return true;
@@ -50,16 +62,22 @@ public class GoldPlate implements Plate {
 	}
 
 	@Override
+//	returns the price of the Sushi plate as specified 
+//	the prices are based on the colors of the plates
 	public double getPrice() {
 		return (Math.round(100*price))/100.0;
 	}
 
 	@Override
+//	returns the color of the Sushi plate as specified
+//	the colors are specified in the Plate.Color enumeration in the Plate Interface
 	public Color getColor() {
 		return Plate.Color.GOLD;
 	}
 
 	@Override
+//	returns the profit of the Sushi plate
+//	profit = the price of the Sushi plate - the profit of the Sushi object's contents
 	public double getProfit() {
 		if (!hasContents()) {
 			return 0.0;
--- src/a5/RedPlate.java
+++ src/a5/RedPlate.java
@@ -1,5 +1,7 @@
 package a5;
 
+//RedPlate implements the Plate Interface
+//RedPlate is a plate of Sushi with potentially 1 Sushi object
 public class RedPlate implements Plate {
 
 	private double price = 1.0;
@@ -10,6 +12,9 @@ public class RedPlate implements Plate {
 	}
 
 	@Override
+//	gets the content of the Sushi plate
+//	input: no parameter
+//	output: null or 1 Sushi object
 	public Sushi getContents() {
 		if (contents == null) {
 			return null;
@@ -19,6 +24,9 @@ public class RedPlate implements Plate {
 	}
 
 	@Override
+//	removes the contents of the Sushi plate 
+//	output: null or 1 Sushi object
+//	returns the original contents of the plate and then empties the plate
 	public Sushi removeContents() {
 		Sushi oldContents = contents;
 		contents = null;
@@ -26,6 +34,9 @@ public class RedPlate implements Plate {
 	}
 
 	@Override
+//	sets the contents of the Sushi plate to a new Sushi object
+//	input: a new Sushi object
+//	output: void but replaces the contents of the original Sushi plate
 	public void setContents(Sushi s) throws PlatePriceException {
 		if (s == null) {
 			throw new IllegalArgumentException("s is null!");
@@ -37,6 +48,7 @@ public class RedPlate implements Plate {
 	}
 
 	@Override
+//	returns true if the Sushi plate has a Sushi object on it
 	public boolean hasContents() {
 		if (contents != null) {
 			return true;
@@ -46,16 +58,22 @@ public class RedPlate implements Plate {
 	}
 
 	@Override
+//	returns the price of the Sushi plate as specified 
+//	the prices are based on the colors of the plates
 	public double getPrice() {
 		return (Math.round(100*price))/100.0;
 	}
 
 	@Override
+//	returns the color of the Sushi plate as specified
+//	the colors are specified in the Plate.Color enumeration in the Plate Interface
 	public Color getColor() {
 		return Plate.Color.RED;
 	}
 
 	@Override
+//	returns the profit of the Sushi plate
+//	profit = the price of the Sushi plate - the profit of the Sushi object's contents
 	public double getProfit() {
 		if (!hasContents()) {
 			return 0.0;
