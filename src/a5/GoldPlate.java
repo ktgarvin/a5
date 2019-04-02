3399015c849adeafe2599e537cd7456bb1b3e80f
ktshep <ktshep@192.168.0.46>
2019-04-02 16:15:18 -0400

    c

--- src/a5/GoldPlate.java
+++ src/a5/GoldPlate.java
@@ -7,7 +7,7 @@ public class GoldPlate implements Plate {
 	private double price = 5.0;
 	private Sushi contents;
 
-	public GoldPlate(Sushi contents) throws PlatePriceException {
+	public GoldPlate(Sushi contents, double price) throws PlatePriceException {
 		this.contents = contents;
 		if (this.price < 5.0) {
 			throw new IllegalArgumentException("Price must be more than $5.00!");
