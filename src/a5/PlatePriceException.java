760b77756f7d132264bb679bc9b128122986c3b3
ktshep <ktshep@192.168.100.235>
2019-04-02 16:34:12 -0400

    r

--- src/a5/PlatePriceException.java
+++ src/a5/PlatePriceException.java
@@ -2,28 +2,8 @@ package a5;
 
 public class PlatePriceException extends Exception {
 
-	public PlatePriceException() {
-		// TODO Auto-generated constructor stub
-	}
-
 	public PlatePriceException(String message) {
-		super(message);
-		// TODO Auto-generated constructor stub
-	}
-
-	public PlatePriceException(Throwable cause) {
-		super(cause);
-		// TODO Auto-generated constructor stub
-	}
-
-	public PlatePriceException(String message, Throwable cause) {
-		super(message, cause);
-		// TODO Auto-generated constructor stub
-	}
-
-	public PlatePriceException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
-		super(message, cause, enableSuppression, writableStackTrace);
-		// TODO Auto-generated constructor stub
+		super("Illegal plate price");
 	}
 
 }
